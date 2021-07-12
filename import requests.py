92981d56af878bf4d9259e019b8be3d6b0ed264d
MennoM1990 <mennominkes@hotmail.com>
2021-07-12 14:20:09 +0200

    added outfile

--- import requests.py
+++ import requests.py
@@ -1,9 +1,22 @@
 import requests
+import json
 from requests.models import Response
 
+
+#API call
 url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"status code: {r.status_code}")
 response_dict = r.json()
 print(response_dict.keys())
+
+#Writing to a file
+with open ('repo.txt', 'w') as outfile:
+    json.dump (response_dict, outfile)
+
+
+
+
+#Explore information about the repositories
+print(f"repositories returned: {len(response_dict)}")
