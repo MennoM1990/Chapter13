0c95a056ae46a826ea7110de17d2ed5172d86277
MennoM1990 <mennominkes@hotmail.com>
2021-07-12 14:06:49 +0200

    Added API-call

---
+++ import requests.py
@@ -0,0 +1,9 @@
+import requests
+from requests.models import Response
+
+url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"status code: {r.status_code}")
+response_dict = r.json()
+print(response_dict.keys())
