4d84acf2c911d8c0c8a6ad967c117d971b6fa7f8
MennoM1990 <mennominkes@hotmail.com>
2021-07-12 14:52:19 +0200

    changed import requests

--- import requests.py
+++ import requests.py
@@ -9,14 +9,21 @@ headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"status code: {r.status_code}")
 response_dict = r.json()
-print(response_dict.keys())
-
-#Writing to a file
-with open ('repo.txt', 'w') as outfile:
-    json.dump (response_dict, outfile)
+print(f"repositories returned: {response_dict['total_count']}")
 
 
+#Writing to a readable file
+readable_file = r"C:\Users\DELL\Desktop\Python Training\With Git\readable_repo_data.json"
+with open (readable_file, 'w') as outfile:
+    json.dump(response_dict, outfile, indent=4)
 
 
 #Explore information about the repositories
-print(f"repositories returned: {len(response_dict)}")
+repo_dicts = response_dict['items']
+print(f"Repositories returned {len(repo_dicts)}")
+
+#Examine the first repository
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key,value in sorted(repo_dict.items()):
+    print(key, value)
